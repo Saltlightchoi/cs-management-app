6de669d413533ebac74e5a7a33d864e9751bc2f4
Saltlightchoi <cyg2476@gmail.com>
2026-07-23 07:55:42 +0900

    voclist

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4095458711" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "640360064" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
