47c422cfb7d7a0fa2f4d635e6fdf6178a303c49c
Saltlightchoi <cyg2476@gmail.com>
2026-07-27 14:59:23 +0900

    up3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1290578462" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2410525957" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
